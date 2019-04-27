627a4ad0700ab3b004cbda7ec71055d580039d8f
m.over <m.over@iplabs.de>
2019-04-27 13:34:21 +0200

    Add Patient constructor

---
+++ src/game/Patient.js
@@ -0,0 +1,11 @@
+
+
+function Patient(x, y, health, wealth, sickness) {
+  this.x = x;
+  this.y = y;
+  this.health = health;
+  this.wealth = wealth;
+  this.sickness = sickness;
+  this.diagnosed = false;
+}
+
