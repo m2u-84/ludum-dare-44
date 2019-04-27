64659ca66b0c9778ef32c029e6c291032aaffd6e
Nils Kreutzer <nkr@netempire.de>
2019-04-27 17:08:56 +0200

    conflict resolved

--- src/game/Tile.js
+++ src/game/Tile.js
@@ -1,17 +1,5 @@
-function Tile(x, y, collide, isBed) {
+function Tile(x, y, collide) {
     this.position = [x, y];
     this.collides = collide;
-    this.isBed = isBed;
-}
-
-Tile.prototype.updatePosition = function(x, y) {
-    this.position = [x, y];
-}
-
-Tile.prototype.updateCollision = function(collide) {
-    this.collides = collide;
-}
-
-Tile.prototype.updateIsBed = function(isBed) {
-    this.bed = bed;
+    this.bed = null;
 }
