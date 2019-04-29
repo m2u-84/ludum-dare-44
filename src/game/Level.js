d1da58592a9eb75fa244f3a56dbda4b87a974fe9
But, Eduard <e.but@iplabs.de>
2019-04-29 15:52:11 +0200

    adds street to left side of hotel

--- src/game/Level.js
+++ src/game/Level.js
@@ -6,27 +6,27 @@ function Level() {
     // use w for wall, b for bed, r for receptionPoint, s for spawnPoint
     // cf. map_raster.png
     let rawMap =
-          '-----w--------------w-----\n'
-        + '-----w--------------w-----\n'
-        + '-----w--------------w-----\n'
-        + '-----wwwwwww--wwwwwww-----\n'
-        + '-----wwb-b-w--w-b-bww-----\n'
-        + '-----w-b-b-wwww-b-b-w-Fww-\n'
-        + '-----w--------------wwwww-\n'
-        + '-----wwwwwww--wwwwwww-p---\n'
-        + '-----wwb-b-w--w-b-bww-----\n'
-        + '-----w-b-b-w--w-b-b-w-----\n'
-        + 'wwwwww--------------ww-www\n'
-        + '-----wwww-ww--ww-wwww-----\n'
-        + '-----w--w-w----w-w--w-----\n'
-        + '-----w--w-wwwwww-wf-w-----\n'
-        + '-----w--w--rrrr-----w-----\n'
-        + '-----wwwww------wwwww-----\n'
-        + '---------www--www---------\n'
-        + '---------www--www---------\n'
-        + 's------------------------s\n'
-        + '--------------------------\n'
-        + '--------------------------',
+          '--sw--------------w-----\n'
+        + '---w--------------w-----\n'
+        + '---w--------------w-----\n'
+        + '---wwwwwww--wwwwwww-----\n'
+        + '---wwb-b-w--w-b-bww-----\n'
+        + '---w-b-b-wwww-b-b-w-Fww-\n'
+        + '---w--------------wwwww-\n'
+        + '---wwwwwww--wwwwwww-p---\n'
+        + '---wwb-b-w--w-b-bww-----\n'
+        + '---w-b-b-w--w-b-b-w-----\n'
+        + '---w--------------ww-www\n'
+        + '---wwww-ww--ww-wwww-----\n'
+        + '---w--w-w----w-w--w-----\n'
+        + '---w--w-wwwwww-wf-w-----\n'
+        + '---w--w--rrrr-----w-----\n'
+        + '---wwwww------wwwww-----\n'
+        + '-------www--www---------\n'
+        + '-------www--www---------\n'
+        + '-----------------------s\n'
+        + '------------------------\n'
+        + '------------------------',
         x, y, collide;
 
     rawMap = rawMap.split('\n');
