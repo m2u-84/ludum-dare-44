aefd3903d2d7c3ea42e7e23c8e6e76ab1baf2cb7
m.over <m.over@iplabs.de>
2019-04-29 15:56:12 +0200

    Change diagnosis cheat key to i to prevent running conflicts

--- src/stages/GameStage.js
+++ src/stages/GameStage.js
@@ -209,7 +209,7 @@ GameStage.prototype.onkey = function (event) {
         this.gameState.patients.forEach( p => p.die() );
     } else if (event.key == "f") {
       this.transitionIn("fracture", 300, { patient: new Patient(5, 5, 100, 50, this.gameState.sicknesses[4], this.gameState)})
-    } else if (event.key == "d") {
+    } else if (event.key == "i") {
       // Diagnose all patients
       this.gameState.patients.forEach( p => p.diagnosed = true );
     }
