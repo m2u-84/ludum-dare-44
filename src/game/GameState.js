5e12004d030dc440083530e68e0632fbfbf5a5f7
m.over <m.over@iplabs.de>
2019-04-29 12:52:31 +0200

    Add patient sleep state

--- src/game/GameState.js
+++ src/game/GameState.js
@@ -7,14 +7,14 @@ function GameState() {
 
     // possible treatments
     this.treatments = {
-        //                            name       price for pat.  hosp.  failure
-        drugs:          new Treatment('Prescribe Drugs',     100,   10,  0.0,  0.0),
-        placeboSurgery: new Treatment('Placebo Surgery',    1000,   80,  0.0,  0.0),
-        surgery:        new Treatment('Proper Surgery',     1000,  500, -0.1, -0.3),
-        organ:          new Treatment('Give Organ',         5000, 1000, -0.1, -0.3, () => this.hospital.organs > 0),
-        antibiotics:    new Treatment('Give Antibiotics',    200,   40, -0.1, -0.1),
-        takeOrgan:      new Treatment('Take Organ',         2000,  500, -0.2, -0.4, (p) => p.hasOrgan),
-        fixLeg:         new Treatment('Fix Fracture',        800,  220, -0.1, -0.3),
+        //                            name        sleepTime[s]   price pat|hosp  failure
+        drugs:          new Treatment('Prescribe Drugs',     5,      100,   10,  0.0,  0.0),
+        placeboSurgery: new Treatment('Placebo Surgery',    10,     1000,   80,  0.0,  0.0),
+        surgery:        new Treatment('Proper Surgery',     20,     1000,  500, -0.1, -0.3),
+        organ:          new Treatment('Give Organ',         30,     5000, 1000, -0.1, -0.3, () => this.hospital.organs > 0),
+        antibiotics:    new Treatment('Give Antibiotics',    5,      200,   40, -0.1, -0.1),
+        takeOrgan:      new Treatment('Take Organ',         30,     2000,  500, -0.2, -0.4, (p) => p.hasOrgan),
+        fixLeg:         new Treatment('Fix Fracture',       20,      800,  220, -0.1, -0.3),
         // comment out the following line if release of patient by doctor should be disabled
         release:        new Treatment('Release as cured',      0,    0,  0.0,  0.0, (p) => p.isHealthy()) // release as cured only if patient is cured
     };
