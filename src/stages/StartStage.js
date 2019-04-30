b040ce8fd16a069c505bb79a89a55ae07caf5548
m.over <m.over@iplabs.de>
2019-04-30 02:46:02 +0200

    Credits in start screen

--- src/stages/StartStage.js
+++ src/stages/StartStage.js
@@ -1,6 +1,9 @@
 
 function StartStage() {
   Stage.call(this, "start", 4);
+  this.creditsText = "Sick Hospital - We Care a Lot (About Money)   is a contribution to Ludum Dare Game Jam Contest #44. " + 
+      "Created by Eduard But, Bjoern Kopiske, Nils Kreutzer, Gordon Lawrenz, Markus Over, Olav Schettler, Jennifer van Veen, " + 
+      "and Matthias Wetter, within 72 hours.   Special thanks to ip.labs GmbH for location and coffee.";
 }
 inherit(StartStage, Stage);
 
@@ -17,6 +20,11 @@ StartStage.prototype.render = function(ctx, timer) {
   }
 
   drawImage(ctx, this.menuImage, 0, 0, 0, 1, 1, 0, 0);
+
+  // Credits Text
+  const off = (this.time / 12) % 2600;
+  const cx = Math.round(w + 100 - off); 
+  mainFont.drawText(ctx, this.creditsText, cx, h - 20, "gray", 0);
 };
 
 StartStage.prototype.onkey = function(event) {
