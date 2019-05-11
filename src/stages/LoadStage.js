4930b11db4850828d17f023fdf06fd6b211b0f3b
Matthias Wetter <m.wetter@iplabs.de>
2019-05-11 23:49:50 +0200

    Added progress bar to initial screen; added social media cover for GitHub.

--- src/stages/LoadStage.js
+++ src/stages/LoadStage.js
@@ -8,23 +8,31 @@ LoadStage.prototype.preload = function() {
 }
 
 LoadStage.prototype.render = function(ctx, timer) {
-  const w = ctx.canvas.width, h = ctx.canvas.height;
-  const p = Interpolators.cubic3(this.opacity);
-  if (p < 1) {
-    ctx.translate(0, -(1 - p) * (h + 10));
-  }
-  // Bar at the bottom
-  ctx.fillStyle = "#666";
-  ctx.fillRect(0, h + 1, w, 4);
-  // Black background
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, w, h);
-  // Message
-  const alpha = (0.7 + 0.2 * Math.sin(timer.runTime * 0.003));
-  ctx.globalAlpha = alpha;
-  ctx.fillStyle = "white";
-  ctx.textAlign = "right";
-  ctx.font = "32px Arial";
-  const percentage = Math.round(loader.progress * 100) + "%";
-  ctx.fillText("Loading " + percentage, w - 30, h - 30);
+    const w = ctx.canvas.width, h = ctx.canvas.height;
+    const p = Interpolators.cubic3(this.opacity);
+
+    if (p < 1) {
+        ctx.translate(0, -(1 - p) * (h + 10));
+    }
+
+    // Black background
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, w, h);
+
+    const percent = Math.round(loader.progress * 100);
+
+    // Progress bar
+    ctx.fillStyle = 'white';
+    ctx.fillRect(20, h - 35, (w - 160) / 100 * percent, 15)
+
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = 'white';
+    ctx.strokeRect(20, h - 35, w - 160, 15);
+
+    // Message
+    const alpha = (0.7 + 0.2 * Math.sin(timer.runTime * 0.003));
+    ctx.globalAlpha = alpha;
+    ctx.textAlign = 'right';
+    ctx.font = '16px Arial';
+    ctx.fillText(`Loading ${percent} %`, w - 20, h - 22);
 };
