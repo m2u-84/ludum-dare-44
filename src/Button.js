d9551bd5ac1d8520a93ef5f20ee933baeac8dc02
Edu <eduke@gmx.de>
2019-05-14 22:15:59 +0200

    Disable multiple clicks on buttons that are single click only

--- src/Button.js
+++ src/Button.js
@@ -17,7 +17,7 @@
  * @param {Sound} hoverSound    Option click sound
  */
 
-function Button(image, frames, action, clickSound = null, hoverSound = null) {
+function Button(image, frames, action, clickSound = null, hoverSound = null, isOneClick = true) {
   this.image = image;
   this.frames = frames;
   this.hoverSound = hoverSound;
@@ -26,6 +26,8 @@ function Button(image, frames, action, clickSound = null, hoverSound = null) {
   this.frame = 0;
   this.hovered = false;
   this.armed = false;
+  this.isOneClick = isOneClick;
+  this.clickedOnce = false;
 }
 
 Button.prototype.isHovered = function(x, y, offsetX, offsetY) {
@@ -48,9 +50,13 @@ Button.prototype.isHovered = function(x, y, offsetX, offsetY) {
 Button.prototype.paint = function(ctx, x, y, offsetX = 0, offsetY = 0) {
 
     if (this.hovered && this.armed && !mouseHandler.mouse.click) {
-      if (this.clickSound) this.clickSound.play();
-      this.action();
-      this.armed = false;
+      if (!this.isOneClick || (this.isOneClick && !this.clickedOnce)) {
+        if (this.clickSound) this.clickSound.play();
+        this.action();
+        this.armed = false;
+        this.clickedOnce = true;
+    }
+
     } else if (this.isHovered(x,y,offsetX,offsetY) && this.hovered && mouseHandler.mouse.click) {
       this.armed = true;
       this.frame = getArrayFrame(timer.gameTime / this.frames.armedSpeed, this.frames.armed);
